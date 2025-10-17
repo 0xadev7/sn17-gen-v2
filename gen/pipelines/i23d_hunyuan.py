b148ded9e11f45ffd1c4809fcdb583c5cd9af184
0xadev7 <leric7510@gmail.com>
2025-10-17 11:38:18 -0400

    fix: hy2.1 import

--- gen/pipelines/i23d_hunyuan.py
+++ gen/pipelines/i23d_hunyuan.py
@@ -4,9 +4,13 @@ from typing import Optional
 from PIL import Image
 from trimesh.exchange.ply import export_ply
 import torch
+import sys
 
-from hy3dpaint.textureGenPipeline import Hunyuan3DPaintPipeline, Hunyuan3DPaintConfig
-from hy3dshape.hy3dshape.pipelines import Hunyuan3DDiTFlowMatchingPipeline
+sys.path.insert(0, './hy3dshape')
+sys.path.insert(0, './hy3dpaint')
+
+from textureGenPipeline import Hunyuan3DPaintPipeline, Hunyuan3DPaintConfig
+from hy3dshape.pipelines import Hunyuan3DDiTFlowMatchingPipeline
 
 
 class HunYuanImageTo3D:
